7cdad1219d154b31baefa035674e3de70f77a560
JoelSolerHuix <81635209+JoelSolerHuix@users.noreply.github.com>
2021-06-01 06:58:42 +0200

    Update ModelKnnHu.m

--- ModelKnnHu.m
+++ ModelKnnHu.m
@@ -1,32 +1,31 @@
-function etiquetes_ret = ModelKnnHu(imgs, X, labels, k)
+function etiquetes_ret = ModelKnnHu(imgs, X, labels, k, s)
 
 hu_inv_train = zeros(60000,35);
 im_veins = zeros(k);
 etiquetes = zeros(k);
-etiquetes_ret = zeros(1000, 1);
+etiquetes_ret = zeros(10000, 1);
 
-for i = 1:60000          
+for i = 1:60000        
     temp = reshape(X(i,:), 28, 28);
     temp2 = soroll(temp, 1);
-    temp_fin = treu_soroll(temp2, 1);   %Posar que el soroll es passi com a paràmetre
+    temp_fin = treu_soroll(temp2, s); 
         
-    hu_junt1 = parteix_i_hu(temp_fin);
+    hu_junt1 = parteix_i_hu(temp);
     hu_inv_train(i,:) = hu_junt1;
     
 end
 
 
-for i = 1:1000
+for i = 1:10
     distancies = zeros(60000,1);
     img = reshape(imgs(i,:), 28, 28);
     hu_junt2 = parteix_i_hu(img);
     
     for j = 1:60000
         distancia = pdist2(hu_junt2, hu_inv_train(j,:));
+        distancies(j) = distancia;
     end
     
-    distancies(i) = distancia;
-
     ordenat = sort(distancies);
     for j = 1:k
         for t = 1:60000
@@ -40,7 +39,7 @@ for i = 1:1000
         etiquetes(p) = labels(im_veins(p));
     end
 
-    etiquetes_ret(i) = mode(etiquetes(1));
+   etiquetes_ret(i) = mode(etiquetes(1));
 end
 
 
